6f7f45ed20d6a46e0b69ac74471c2782ce66b528
LUCAS <lucarty79@gmail.com>
2026-04-06 17:14:28 -0300

    portfolio 06/04/2026

---
+++ scipt/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#teste").click(function(){
+        alert("teste, botao clicado!")
+    
+        // abrir automatiocamente
+    });
+    // $("#staticBackdrop").modal("show");
+    });
